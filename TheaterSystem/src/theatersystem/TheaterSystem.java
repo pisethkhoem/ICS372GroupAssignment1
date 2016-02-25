9b868ecff37e7572d0d0f62263dd0a37b1944e3f
evenl <evenl@DCLARK.hsd1.mn.comcast.net>
2016-02-24 22:08:35 -0600

    main file for the theater project

---
+++ TheaterSystem/src/theatersystem/TheaterSystem.java
@@ -0,0 +1,20 @@
+package theatersystem;
+
+/**
+ * The main class for the theater project.
+ * 
+ * @author Daniel Clark
+ * @author Jared Johnson
+ * @author Piseth Koehm
+ * @author Robert Novak
+ */
+public class TheaterSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("You successfully got our git project wooo!");
+    }
+
+}
